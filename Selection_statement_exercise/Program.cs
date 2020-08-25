5350290e55d8d7a98a23a54f8b2161a747020ede
Bryce <ImperialEagle1998@gmail.com>
2020-08-25 11:20:25 -0500

    Add project files.

---
+++ Selection_statement_exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Selection_statement_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low!, try again!");
+            }
+
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine("That is WAY too high a guess.");
+            }
+
+            else
+            {
+                Console.WriteLine("... errrrrrr... not totally sur ehow you came up with that but.. ya know what? \n Nevermind.");
+            }
+
+        }
+    }
+}
